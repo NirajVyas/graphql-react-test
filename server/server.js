6deed5f0957ee015734b62b3e593db29df0105fa
Niraj Vyas <>
2021-01-20 12:01:38 +0000

    initial mongo setup

--- server/server.js
+++ server/server.js
@@ -10,16 +10,18 @@ const envData = dotenv.config().parsed;
 
 const app = express();
 
-const MONGO_URI = `mongodb+srv://admin:${envData.DB_PASSWORD}@graphql-react-test.yn8zf.mongodb.net/${envData.DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://admin:${envData.DB_PASSWORD}@graphql-react-test.yn8zf.mongodb.net/?retryWrites=true&w=majority`;
+
+
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error('You must provide a Mongo URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('open', () => console.log('Connected to Mongo instance.'))
+    .on('error', error => console.log('Error connecting to Mongo:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
