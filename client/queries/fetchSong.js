383812c12c38c41ffe1c534584d6259e22712a8d
Niraj Vyas <>
2021-01-22 16:07:09 +0000

    added queries

---
+++ client/queries/fetchSong.js
@@ -0,0 +1,18 @@
+
+import gql from 'graphql-tag';
+
+export default gql`
+  query SongQuery($id: ID!) {
+    song(id: $id)  {
+      title
+      id
+      lyrics {
+        id
+        content
+        likes
+      }
+    }
+  }
+`
+
+
